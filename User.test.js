4c7bbea0eea5a6a51049b31ff4a777cbb0c3704f
Connor Frendt <cjacksonfx@yahoo.com>
2023-08-10 15:19:29 -0700

    wip

--- User.test.js
+++ User.test.js
@@ -4,5 +4,4 @@ const { Types } = require('mongoose');
 it('validates a good model', () => {
     const user = new User({name: 'Concon', favNumber: 15});
     expect(user.toJSON()).toEqual({ name: 'Concon', favNumber: 15, _id: expect.any(Types.ObjectId) });
-
 });
